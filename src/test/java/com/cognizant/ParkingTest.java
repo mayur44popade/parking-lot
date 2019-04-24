a89b41590782978074052e1ac09ea7eaf210fc7a
mayur44popade <mayur44popade@gmail.com>
2019-04-24 15:38:15 -0400

    First commit

---
+++ src/test/java/com/cognizant/ParkingTest.java
@@ -0,0 +1,15 @@
+package com.cognizant;
+
+import org.junit.Test;
+import org.junit.Assert;
+public class ParkingTest {
+
+    public void setUp(){
+        Parking p = new Parking();
+    }
+
+    @Test
+    public void registerCarAndCheckIfItExists(){
+        
+    }
+}
